48a6912b51fe52e3d6fde5b639696f12eb0eb9c2
ablyss <jb.epluribusunix.net>
2026-04-03 10:47:41 -0400

    update build

--- src/rakarrack.h
+++ src/rakarrack.h
@@ -15,30 +15,6 @@
 #include <FL/Fl_File_Chooser.H>
 #include <FL/Fl_Color_Chooser.H>
 
-
-
-/* Implementation of strsep for Haiku/non-BSD systems */
-#include <cstring>
-static inline char *strsep(char **stringp, const char *delim) {
-    char *s;
-    const char *spanp;
-    int c, sc;
-    char *tok;
-    if ((s = *stringp) == NULL) return (NULL);
-    for (tok = s;;) {
-        c = *s++;
-        spanp = delim;
-        do {
-            if ((sc = *spanp++) == c) {
-                if (c == 0) s = NULL;
-                else s[-1] = 0;
-                *stringp = s;
-                return (tok);
-            }
-        } while (sc != 0);
-    }
-}
-
 class Analyzer : public Fl_Box {
 public:
   Analyzer(int x,int y, int w, int h, const char *label=0);
