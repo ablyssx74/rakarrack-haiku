20d50e1e266ddfe9e828dc87b739fbacd784e910
ablyss <jb.epluribusunix.net>
2026-04-16 21:03:15 -0400

    Update

--- haiku_native/haiku-rakarrack.cpp
+++ haiku_native/haiku-rakarrack.cpp
@@ -28,6 +28,8 @@
 #include <MenuItem.h>
 #include <Message.h>
 #include <PopUpMenu.h>
+#include <ScrollView.h>
+#include <Size.h>
 #include <StringView.h>
 #include <Slider.h>
 #include <StringView.h>
@@ -44,7 +46,6 @@
 
 extern pthread_mutex_t jmutex;
 
-
 enum {
     MSG_OVRD_TOGGLE = 'ovtg',
     MSG_OVRD_DRIVE  = 'ovdr',
@@ -54,9 +55,13 @@ enum {
     MSG_OVRD_TYPE   = 'ovtp',
     MSG_ECHO_TOGGLE = 'ectg', 
     MSG_ECHO_DELAY  = 'ecdl',
-    MSG_ECHO_FB     = 'ecfb' 
+    MSG_ECHO_FB     = 'ecfb', 
+    MSG_MASTER_FX   = 'mfxs',
+    MSG_MASTER_IN_GAIN  = 'mign',
+    MSG_MASTER_OUT_GAIN = 'mogn'
 };
 
+// Main View
 class RakarrackView : public BView {
 public:
     RakarrackView(RKR* rkr) : BView("MainView", B_WILL_DRAW | B_PULSE_NEEDED) {
@@ -66,11 +71,22 @@ public:
         fCpuDisplay = new BStringView("cpu", "CPU: 0.00%");
         fCpuDisplay->SetHighColor(100, 0, 0);
 
-        // 1. Create the Preset Menu
-        BPopUpMenu* typeMenu = new BPopUpMenu("Select Type");
+        // --- Master FX Toggle ---
+        fMasterFX = new BCheckBox("master_fx", "FX Engine", new BMessage(MSG_MASTER_FX));
+        if (fRkr->Bypass == 1) {
+    		fMasterFX->SetValue(B_CONTROL_ON);
+		} else {
+    		fMasterFX->SetValue(B_CONTROL_OFF);
+		}
+        BFont boldFont(be_bold_font);
+        fMasterFX->SetFont(&boldFont);
         
-        // We add items and attach a message to each. 
-        // We can put the type index (0, 1, 2...) directly in the message.
+        // Sync initial state: 0 = On (Checked), 1 = Bypass (Unchecked)
+        if (fRkr->Bypass == 0)
+            fMasterFX->SetValue(B_CONTROL_ON);
+
+        // --- Overdrive Section ---
+        BPopUpMenu* typeMenu = new BPopUpMenu("Select Type");
         BMessage* msg1 = new BMessage(MSG_OVRD_TYPE);
         msg1->AddInt32("index", 0);
         typeMenu->AddItem(new BMenuItem("Overdrive 1", msg1));
@@ -86,21 +102,41 @@ public:
         
         BGroupView* ovrdCnt = new BGroupView(B_VERTICAL, 5);
         ovrdCnt->GroupLayout()->SetInsets(10);
-        
         ovrdCnt->AddChild(new BCheckBox("On", "On", new BMessage(MSG_OVRD_TOGGLE)));
-        ovrdCnt->AddChild(typeSelector); // Add the dropdown here
+        ovrdCnt->AddChild(typeSelector);
         ovrdCnt->AddChild(new BSlider("drive", "Drive", new BMessage(MSG_OVRD_DRIVE), 0, 127, B_HORIZONTAL));
         ovrdCnt->AddChild(new BSlider("level", "Level", new BMessage(MSG_OVRD_LEVEL), 0, 127, B_HORIZONTAL));
         ovrdCnt->AddChild(new BSlider("lpf", "LPF", new BMessage(MSG_OVRD_LPF), 0, 127, B_HORIZONTAL));
         ovrdCnt->AddChild(new BSlider("hpf", "HPF", new BMessage(MSG_OVRD_HPF), 0, 127, B_HORIZONTAL));
-        
         ovrdBox->AddChild(ovrdCnt);
 
-        BLayoutBuilder::Group<>(this, B_VERTICAL, 10)
+        // --- Echo Section ---
+        BBox* echoBox = new BBox("Echo");
+        echoBox->SetLabel("Echo");
+        
+        BGroupView* echoCnt = new BGroupView(B_VERTICAL, 5);
+        echoCnt->GroupLayout()->SetInsets(10);
+        echoCnt->AddChild(new BCheckBox("On", "On", new BMessage(MSG_ECHO_TOGGLE)));
+        echoCnt->AddChild(new BSlider("delay", "Delay", new BMessage(MSG_ECHO_DELAY), 0, 127, B_HORIZONTAL));
+        echoCnt->AddChild(new BSlider("fb", "Feedback", new BMessage(MSG_ECHO_FB), 0, 127, B_HORIZONTAL));
+        echoBox->AddChild(echoCnt);
+
+
+        // --- Layout: Horizontal Rack Style ---
+        BLayoutBuilder::Group<>(this, B_VERTICAL, 0)
             .SetInsets(10)
-            .Add(fCpuDisplay)
-            .Add(ovrdBox)
-            .AddGlue();
+            .AddGroup(B_HORIZONTAL, 20)
+                .Add(fCpuDisplay)
+                .Add(fMasterFX)
+                .AddGlue()
+            .End()
+            .AddStrut(10)
+            .AddGroup(B_HORIZONTAL, 10) 
+                .Add(ovrdBox)
+                .Add(echoBox)
+                .AddGlue() 
+            .End()
+            .AddGlue(); 
     }
 
     virtual void Pulse() {
@@ -113,52 +149,92 @@ public:
 private:
     RKR* fRkr;
     BStringView* fCpuDisplay;
+    BCheckBox* fMasterFX;
 };
 
 class RakarrackWindow : public BWindow {
 public:
     RakarrackWindow(BRect frame, RKR* rkr) 
         : BWindow(frame, "Rakarrack Native", B_TITLED_WINDOW, 
-          B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE | B_AUTO_UPDATE_SIZE_LIMITS) {
+          B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE) {
         
         fRkr = rkr;
         SetLayout(new BGroupLayout(B_VERTICAL));
-        AddChild(new RakarrackView(rkr));
+
+        RakarrackView* mainView = new RakarrackView(rkr);
+        mainView->SetExplicitMinSize(BSize(300, 200));
+
+        BScrollView* scroller = new BScrollView("rack_scroll", mainView, 0, true, true);
+
+        BLayoutBuilder::Group<>(this, B_VERTICAL, 0)
+            .Add(scroller)
+            .End();
+
+        this->SetSizeLimits(200, 10000, 150, 10000);
+        SetPulseRate(50000);
     }
 
-
-virtual void MessageReceived(BMessage* msg) {
-    pthread_mutex_lock(&jmutex);
-    
-    switch (msg->what) {
-        // --- Distortion Dashboard ---
-        case MSG_OVRD_DRIVE:
-            if (fRkr->efx_Overdrive)
-                fRkr->efx_Overdrive->Pdrive = (int)msg->GetInt32("be:value", 90);
-            break;
-        case MSG_OVRD_LEVEL:
-            if (fRkr->efx_Overdrive)
-                fRkr->efx_Overdrive->Plevel = (int)msg->GetInt32("be:value", 64);
-            break;
-
-case MSG_ECHO_DELAY:
-    if (fRkr->efx_Echo)
-        fRkr->efx_Echo->Pdelay = (int)msg->GetInt32("be:value", 0);
-    break;
-
-case MSG_ECHO_FB:
-    if (fRkr->efx_Echo)
-        fRkr->efx_Echo->Pfb = (int)msg->GetInt32("be:value", 0); // Changed to Pfb
-    break;
-
-
-        default:
-            BWindow::MessageReceived(msg);
+    virtual void MessageReceived(BMessage* msg) {
+        pthread_mutex_lock(&jmutex);
+        
+        switch (msg->what) {
+            case MSG_MASTER_FX:
+            {
+                int32 value = msg->GetInt32("be:value", B_CONTROL_OFF);
+    if (value == B_CONTROL_ON) {
+        // Based on your test: Checkbox ON needs Bypass = 1
+        fRkr->Bypass = 1; 
+        fRkr->calculavol(1);
+        fRkr->calculavol(2);
+        fRkr->booster = 1.0f;
+        printf("[DEBUG] FX ON (Bypass set to 1)\n");
+    } else {
+        // Checkbox OFF needs Bypass = 0
+        fRkr->Bypass = 0;
+        printf("[DEBUG] FX OFF (Bypass set to 0)\n");
     }
-    
-    pthread_mutex_unlock(&jmutex);
-}
-
+                break;
+            }
+            case MSG_OVRD_TOGGLE:
+                fRkr->Overdrive_Bypass = !fRkr->Overdrive_Bypass;
+                break;
+            case MSG_OVRD_TYPE:
+                if (fRkr->efx_Overdrive)
+                    fRkr->efx_Overdrive->Ptype = msg->FindInt32("index");
+                break;
+            case MSG_OVRD_DRIVE:
+                if (fRkr->efx_Overdrive)
+                    fRkr->efx_Overdrive->Pdrive = (int)msg->GetInt32("be:value", 90);
+                break;
+            case MSG_OVRD_LEVEL:
+                if (fRkr->efx_Overdrive)
+                    fRkr->efx_Overdrive->Plevel = (int)msg->GetInt32("be:value", 64);
+                break;
+            case MSG_OVRD_LPF:
+                if (fRkr->efx_Overdrive)
+                    fRkr->efx_Overdrive->Plpf = (int)msg->GetInt32("be:value", 127);
+                break;
+            case MSG_OVRD_HPF:
+                if (fRkr->efx_Overdrive)
+                    fRkr->efx_Overdrive->Phpf = (int)msg->GetInt32("be:value", 0);
+                break;
+            case MSG_ECHO_TOGGLE:
+                fRkr->Echo_Bypass = !fRkr->Echo_Bypass;
+                break;
+            case MSG_ECHO_DELAY:
+                if (fRkr->efx_Echo)
+                    fRkr->efx_Echo->Pdelay = (int)msg->GetInt32("be:value", 0);
+                break;
+            case MSG_ECHO_FB:
+                if (fRkr->efx_Echo)
+                    fRkr->efx_Echo->Pfb = (int)msg->GetInt32("be:value", 0);
+                break;
+            default:
+                BWindow::MessageReceived(msg);
+        }
+        
+        pthread_mutex_unlock(&jmutex);
+    }
 
 private:
     RKR* fRkr;
@@ -166,7 +242,7 @@ private:
 
 extern "C" void start_haiku_native_interface(void* rkr_ptr) {
     RKR* rkr = (RKR*)rkr_ptr;
-    RakarrackWindow *win = new RakarrackWindow(BRect(100, 100, 500, 500), rkr);
+    RakarrackWindow *win = new RakarrackWindow(BRect(100, 100, 550, 400), rkr);
     win->Show();
 }
 
